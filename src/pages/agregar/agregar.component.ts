4c3ba55b289636b8dc7c96bb55a79eb2536d9638
Andres <jhonatan.leon@farmatodo.com>
2018-12-06 13:35:11 -0500

    se agrega pantalla de agregar

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,5 +1,9 @@
 import { Component } from "@angular/core";
 import { DeseosService } from '../../services/deseos.service';
+import { NavParams } from 'ionic-angular';
+import { Lista, ListaItem } from '../../models';
+
+
 
 @Component({
     selector: 'page-agregar',
@@ -7,9 +11,37 @@ import { DeseosService } from '../../services/deseos.service';
 })
 export class AgregarPage {
     
-    constructor ( public _deseosService: DeseosService){
+	lista: Lista;
+	nombreItem: string = '';
+
+    constructor ( public _deseosService: DeseosService,
+    			  private navParams: NavParams
+    		){
+
+
+    	const titulo = this.navParams.get('titulo') 
+
+    	console.log( titulo )
+
+    	this.lista = new Lista( titulo )
             
     }
 
+    agregarItem() {
+
+    	if( this.nombreItem.length > 0){
+    		console.log(this.nombreItem)
+    	}
+
+    	const nuevoItem = new ListaItem( this.nombreItem )
+    	this.lista.items.push ( nuevoItem );
+    	this.nombreItem = '';
+
+    	console.log("array",this.lista.items)
+    }
+
+    actualizarTarea( item:ListaItem ){
+    	item.completado = !item.completado;
+    }
 
 }
--- src/pages/pendientes/pendientes.component.ts
+++ src/pages/pendientes/pendientes.component.ts
@@ -1,6 +1,10 @@
 import { Component } from "@angular/core";
 import { DeseosService } from '../../services/deseos.service';
 import { Lista } from '../../models';
+import { NavController, AlertController } from 'ionic-angular';
+import { AgregarPage } from '../agregar/agregar.component';
+
+
 
 @Component({
     selector: 'page-pendientes',
@@ -8,12 +12,47 @@ import { Lista } from '../../models';
 })
 export class PendientesPage {
     
-    constructor ( public _deseosService: DeseosService){
+    constructor ( public _deseosService: DeseosService,
+    			  private navCtrl: NavController,
+                  private alertCtrl: AlertController
+    				){
             
     }
 
     listaSeleccionada ( lista: Lista ){
         console.log( lista )
+    }
+
+    agregarLista(){
+
+        const alerta = this.alertCtrl.create({
+            title: 'Nueva Lista',
+            message: 'Nombre de la nueva lista que desea',
+            inputs: [{
+                name: 'titulo',
+                placeholder: 'Nombre de la lista'
+            }],
+            buttons: [{
+                text: 'Cancelar'
+            },{
+                text: 'Agregar',
+                handler: data=>{
+                    if(data.titulo.length === 0){
+                        return;
+                    }else{
+                        console.log("data",data)
+                        this.navCtrl.push( AgregarPage, {
+                            titulo: data.titulo
+                        } )
+                    }
+                }
+            }]
+        });
+
+        alerta.present();
+
+
+    	//this.navCtrl.push( AgregarPage );
     }
 
 }
